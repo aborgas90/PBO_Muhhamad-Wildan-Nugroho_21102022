f38b5eea7a7c3be7600256aab2d07014bbf16048
aborgas90 <wildanmuh7@gmail.com>
2023-05-24 22:03:24 +0700

    Guided Laprak 6

---
+++ Laprak6/MultipleCatch.java
@@ -0,0 +1,29 @@
+package Laprak6;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MultipleCatch {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            try {
+                System.out.println("Masukkan Angka :");
+                int angka1 = input.nextInt();
+
+                System.out.println("Masukkan Angka Pembagi:");
+                int angka2 = input.nextInt();
+
+                int hasil = angka1 / angka2;
+                System.out.println(angka1 + "/" + angka2 + "=" + hasil + " (dibulatkan)");
+            } catch (ArithmeticException e) {
+                System.out.println("Error: Kesalahan dalam perihitungan");
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Kesalahan dalam input!");
+            }finally{
+                System.out.println("Finally akan selalu dijalankan");
+            }
+            System.out.println("Akhir dari program");
+        }
+    }
+}
+
---
+++ Laprak6/TryCatch.java
@@ -0,0 +1,18 @@
+package Laprak6;
+
+public class TryCatch {
+    public static void main(String[] args) {
+        //membuat array
+        int [] angka = {1,2,3,4,5};
+
+        // //memanggil array
+        try {
+            System.out.println(angka[4]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Index yang dimasukkan melebihi kapasitas");
+        }
+        System.out.println("Akhir dari Program");
+    }
+}
+
+
